9a1bc0eed40b5c05afca69e26c668e900107f5bd
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-21 17:14:54 +0900

    Add files via upload

---
+++ mvkdue.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/update/240911/bgloop.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
